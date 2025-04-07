7cf5f4788ad30573e214db8ed59bfdc64d3a902a
pinjhu <hupinjie@huayimt.com>
2025-04-07 21:33:41 +0800

    [feat]初始化项目

---
+++ src/main/java/com/backend/app/debt/DebtClaimSubmissionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.backend.app.debt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebtClaimSubmissionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DebtClaimSubmissionServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backend/app/debt/DebtClaimSubmissionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.app.debt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebtClaimSubmissionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
